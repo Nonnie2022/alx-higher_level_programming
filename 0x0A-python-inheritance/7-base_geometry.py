360e0a5c7932387d77668df771cdfde67102548a
Nonnie2022 <lindaikedigwe@gmail.com>
2023-02-07 04:24:46 -0800

    Corrected

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+"""BaseGeometry class Module"""
 
 
-class BaseGeometry:
+class BaseGeometry():
     """BaseGeometry class"""
-
     def area(self):
-        """method for calculated area"""
+        """Method Raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method for validate if a num is integer"""
-
-        if type(value) is not int:
+        """Method for validating value"""
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-"""
-===================================
-module with class Rectangle
-===================================
-"""
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
+    """Rectangle class"""
     def __init__(self, width, height):
+        """Initilize rectangle method"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
